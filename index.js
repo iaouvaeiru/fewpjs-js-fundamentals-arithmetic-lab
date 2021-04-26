48ad0889ac4cf6f43f450db4fe4f5ba1a7ab0b7a
Leon Fan <leonkind@gmail.com>
2021-04-26 17:19:13 -0500

    commit tree 414a86cbf428f192af0519a96126d3c44a3c825f

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+let randomDecimal = Math.random()
+let random = parseInt(randomDecimal * 10)
+
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4 
+
+let max = Math.max(1, 2, 3, 4, 20)
